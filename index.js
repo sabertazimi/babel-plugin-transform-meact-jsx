6ec0eba6f3d1e19b58cc926b7a3b98e77ef7c2d4
sabertazimi <sabertazimi@gmail.com>
2018-06-15 22:59:31 +0800

    update(jsx): detect whether jsx exists or not

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = (babel) => {
     const t = babel.types;
+    let isJSXExisted = false;
     let isMeactContextEnabled = false;
 
     return {
         visitor: {
             Program: {
                 exit(path) {
-                    if (isMeactContextEnabled === false) {
+                    if (isJSXExisted === true && isMeactContextEnabled === false) {
                         throw path.buildCodeFrameError(
                             `Meact isn't in current context!`
                         );
@@ -20,6 +21,7 @@ module.exports = (babel) => {
             },
             MemberExpression(path, state) {
                 if (path.node.object.name === 'React' && path.node.property.name === 'createElement') {
+                    isJSXExisted = true;
                     path.replaceWith(t.MemberExpression(t.identifier('Meact') ,t.identifier('createElement')));
                 }
             },
